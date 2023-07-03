c0560c99b8c61b5c6eb60ba33e60fb62fae8cf3d
Milo Trujillo <milo.trujillo@uvm.edu>
2023-07-02 21:21:54 -0400

    Improved noise reduction

--- preprocess.sh
+++ preprocess.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-convert -brightness-contrast 30x100 -alpha off -threshold 50% $1 $2
+convert -white-threshold 25% -brightness-contrast 30x100 -alpha off -threshold 50% $1 $2
+#convert -brightness-contrast 30x100 -alpha off -threshold 70% $1 $2
